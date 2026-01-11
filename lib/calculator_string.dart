4298f113bdb5e567de61aa3eb0672e5951835693
Sindhav Dasharath <dasharathsindhav@gmail.com>
2026-01-11 14:28:42 +0530

    first test of failing

---
+++ lib/calculator_string.dart
@@ -0,0 +1,5 @@
+class CalculatorString {
+  int add(String numbers) {
+    return 0;
+  }
+}
---
+++ test/calculator_string_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:calculator_string_tdd/calculator_string.dart';
+
+void main() {
+  test('returns 0 for empty string', () {
+    final calculator = CalculatorString();
+    final result = calculator.add('');
+    expect(result, 1);
+  });
+}
