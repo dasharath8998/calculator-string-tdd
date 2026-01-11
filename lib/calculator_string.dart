7f62083d5fb14133bc8eb57c827feb393c20cec5
Sindhav Dasharath <dasharathsindhav@gmail.com>
2026-01-11 18:07:34 +0530

    Pass custom delimiter in input

--- lib/calculator_string.dart
+++ lib/calculator_string.dart
@@ -3,8 +3,14 @@ class CalculatorString {
     if (numbers.isEmpty) {
       return 0;
     }
-    numbers = numbers.replaceAll('\n', ',');
-    List<String> parts = numbers.split(',');
+    String delimiter = ',';
+    if (numbers.startsWith('//')) {
+      int indexNewLine = numbers.indexOf('\n');
+      delimiter = numbers.substring(2, indexNewLine);
+      numbers = numbers.substring(indexNewLine + 1);
+    }
+    numbers = numbers.replaceAll('\n', delimiter);
+    List<String> parts = numbers.split(delimiter);
     int sum = 0;
     for (int i = 0; i < parts.length; i++) {
       sum = sum + int.parse(parts[i]);
