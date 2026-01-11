6fed1901da4599bc9267b2ad1dce59337c5c9ab3
Sindhav Dasharath <dasharathsindhav@gmail.com>
2026-01-11 14:50:48 +0530

    Success test case of returns sum of two numbers, with all other cases

--- lib/calculator_string.dart
+++ lib/calculator_string.dart
@@ -3,6 +3,12 @@ class CalculatorString {
     if(numbers.isEmpty) {
       return 0;
     }
+    if (numbers.contains(',')) {
+      List<String> parts = numbers.split(',');
+      int  first = int.parse(parts[0]);
+      int  second = int.parse(parts[1]);
+      return first + second;
+    }
     return int.parse(numbers);
   }
 }
