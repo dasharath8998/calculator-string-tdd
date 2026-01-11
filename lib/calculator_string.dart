d7d12a964c1c761e9deb9fc4292aeb9b3851f8fa
Sindhav Dasharath <dasharathsindhav@gmail.com>
2026-01-11 15:29:57 +0530

    Pass test case "newline with comma", replaced \n to ''

--- lib/calculator_string.dart
+++ lib/calculator_string.dart
@@ -3,6 +3,7 @@ class CalculatorString {
     if (numbers.isEmpty) {
       return 0;
     }
+    numbers = numbers.replaceAll('\n', ',');
     List<String> parts = numbers.split(',');
     int sum = 0;
     for (int i = 0; i < parts.length; i++) {
